b63f83c147efce5ffa442b68d301c7742102f9c0
Andrea Finollo <andrea.finollo@fastwebnet.it>
2016-01-18 20:15:26 +0100

    Initial Commit

---
+++ TestAlignmentRect/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestAlignmentRect
+//
+//  Created by Andrea Finollo on 18/01/16.
+//  Copyright © 2016 Andrea Finollo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
